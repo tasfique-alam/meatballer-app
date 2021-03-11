547e8b34236876a525b944661eee5bcfea7e809e
tasfique-alam <tasfiquealam@gmail.com>
2021-03-11 23:43:10 +0600

    start screen aded

--- src/screens/Welcome/index.js
+++ src/screens/Welcome/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react"
 import { ImageBackground, Image, TouchableOpacity } from "react-native"
-import Block from '../../../components/Block'
-import Text from '../../../components/Text'
-import { PrimaryButton } from '../../../components/Buttons'
+import Block from '../../components/Block'
+import Text from '../../components/Text'
+import { PrimaryButton } from '../../components/Buttons'
 import styles from "./style"
 
 
@@ -15,9 +15,8 @@ export default WelcomeScreen = props => {
   return (
 
     <Block center middle block>
-
       <Text center style={styles.title}>Welcome To</Text>
-      <Image source={require('../../../assets/images/logo_red.png')} />
+      <Image source={require('../../assets/images/logo_red.png')} />
       <Block center>
         <PrimaryButton btnText="login" />
       </Block>
